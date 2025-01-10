85229bd6210eca371d41693a73fd83942771e034
Vicente Guzman <vicente.g.guzman@accenture.com>
2025-01-09 21:59:41 -0600

    Create IStarWarsService.cs

---
+++ StarWars_TabBar/StarWars_TabBar/Services/IStarWarsService.cs
@@ -0,0 +1,12 @@
+﻿using StarWars_TabBar.Models;
+
+namespace StarWars_TabBar.Services
+{
+    public interface IStarWarsService
+    {
+        //Método (Tarea Asincrona)
+        public Task<List<DataModel>> GetPersonajesAsync();
+        public Task<List<DataModel>> GetPlanetsAsync();
+        public Task<List<DataModel>> GetStarShipsAsync();
+    }
+}
