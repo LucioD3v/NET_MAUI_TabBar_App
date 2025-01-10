b8607426c5ded80c8ee4384e7c170239be5f08bb
Vicente Guzman <vicente.g.guzman@accenture.com>
2025-01-09 19:31:29 -0600

    Create DataModel.cs

---
+++ StarWars_TabBar/StarWars_TabBar/Models/DataModel.cs
@@ -0,0 +1,19 @@
+﻿
+namespace StarWars_TabBar.Models
+{
+    public class DataModel  
+    {
+        //Properties of People
+        public string name { get; set; }
+        public string gender { get; set; }
+
+        //Properties of Planets
+        public string climate { get; set; }
+
+        public string terrain { get; set; }
+
+        //Properties of Starships
+        public string model { get; set; }
+        public string starship_class { get; set; }
+    }
+}
