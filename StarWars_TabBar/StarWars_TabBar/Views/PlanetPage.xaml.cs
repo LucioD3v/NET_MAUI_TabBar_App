5f079ab2c7d8d6f54f150669f4569cfbf8fb967b
Vicente Guzman <vicente.g.guzman@accenture.com>
2025-01-15 11:42:50 -0600

    Changes to Planet and StarShip views and logic

--- StarWars_TabBar/StarWars_TabBar/Views/PlanetPage.xaml.cs
+++ StarWars_TabBar/StarWars_TabBar/Views/PlanetPage.xaml.cs
@@ -1,9 +1,24 @@
+using StarWars_TabBar.Services;
+
 namespace StarWars_TabBar.Views;
 
 public partial class PlanetPage : ContentPage
 {
-	public PlanetPage()
-	{
-		InitializeComponent();
-	}
+    private readonly IStarWarsService _starWarsService;
+
+    public PlanetPage(IStarWarsService starWarsService)
+    {
+        _starWarsService = starWarsService;
+        InitializeComponent();
+    }
+
+    private async void OnDataClicked(object sender, EventArgs e)
+    {
+        loading.IsVisible = true;
+
+        var data = await _starWarsService.GetPlanetsAsync();
+        listViewPlanets.ItemsSource = data;
+
+        loading.IsVisible = false;
+    }
 }
--- StarWars_TabBar/StarWars_TabBar/Views/StarShipPage.xaml.cs
+++ StarWars_TabBar/StarWars_TabBar/Views/StarShipPage.xaml.cs
@@ -1,9 +1,23 @@
+using StarWars_TabBar.Services;
+
 namespace StarWars_TabBar.Views;
 
 public partial class StarShipPage : ContentPage
 {
-	public StarShipPage()
+    private readonly IStarWarsService _starWarsService;
+    public StarShipPage(IStarWarsService starWarsService)
 	{
-		InitializeComponent();
+        _starWarsService = starWarsService;
+        InitializeComponent();
 	}
+
+    private async void OnDataClicked(object sender, EventArgs e)
+    {
+        loading.IsVisible = true;
+
+        var data = await _starWarsService.GetStarShipsAsync();
+        listViewStarShips.ItemsSource = data;
+
+        loading.IsVisible = false;
+    }
 }
